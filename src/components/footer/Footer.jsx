09a5d752b1caf16b1a91c15b639a31847856d92d
Jash Mehta <mehtaj8@mcmaster.ca>
2022-11-05 16:38:59 -0400

    Updated App Logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.scss';
 import { Link } from 'react-router-dom';
 
 import bg from '../../assets/footer-bg.jpg';
-import logo from '../../assets/tmovie.png';
+import logo from '../../assets/PopcornMovies.png';
 
 const Footer = () => {
     return (
@@ -14,26 +14,19 @@ const Footer = () => {
                 <div className="footer__content__logo">
                     <div className="logo">
                         <img src={logo} alt="" />
-                        <Link to="/">tMovies</Link>
+                        <Link to="/">PopcornMovies</Link>
                     </div>
                 </div>
                 <div className="footer__content__menus">
                     <div className="footer__content__menu">
                         <Link to="/">Home</Link>
+                    </div>
+                    <div className="footer__content__menu">
                         <Link to="/">Contact us</Link>
-                        <Link to="/">Term of services</Link>
                         <Link to="/">About us</Link>
                     </div>
                     <div className="footer__content__menu">
-                        <Link to="/">Live</Link>
-                        <Link to="/">FAQ</Link>
-                        <Link to="/">Premium</Link>
-                        <Link to="/">Pravacy policy</Link>
-                    </div>
-                    <div className="footer__content__menu">
-                        <Link to="/">You must watch</Link>
-                        <Link to="/">Recent release</Link>
-                        <Link to="/">Top IMDB</Link>
+                        <Link to="/">Term of services</Link>
                     </div>
                 </div>
             </div>
